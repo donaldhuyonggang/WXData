4b9609f632b2296d72dcc7b06c3f1051293eabf6
donaldhuyonggang <65121227@qq.com>
2018-11-23 16:13:18 +0800

    公众号完成

--- WXService/Utility/MyHttpUtility.cs
+++ WXService/Utility/MyHttpUtility.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace WXService.Utility
 {
@@ -12,6 +14,24 @@ namespace WXService.Utility
     /// </summary>
     public static  class MyHttpUtility
     {
+        /// <summary>
+        /// 读取请求对象的内容，只能读一次
+        /// </summary>
+        /// <param name="request">HttpRequest对象</param>
+        /// <returns>string</returns>
+        public static string ReadRequest(HttpRequestBase request)
+        {
+            string reqStr = string.Empty;
+            using (Stream s = request.InputStream)
+            {
+                using (StreamReader reader = new StreamReader(s, Encoding.UTF8))
+                {
+                    reqStr = reader.ReadToEnd();
+                }
+            }
+            return reqStr;
+        }
+
         public static string SendGet(string url)
         {
             return "";
