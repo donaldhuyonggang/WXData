f32fb63a5b20c6fd765146ac5903362620b11c3a
Maple <Maple@MECHREVO-X7TIS>
2018-11-27 15:54:42 +0800

    xcy

--- WXDataUI/Areas/WXUser/Controllers/HomeController.cs
+++ WXDataUI/Areas/WXUser/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WXDataBLL;
+using WXDataModel;
 
 namespace WXDataUI.Areas.WXUser.Controllers
 {
@@ -12,6 +14,33 @@ namespace WXDataUI.Areas.WXUser.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetUsers(int type = 1)
+        {
+            WX_App app = (Session["SYSUSER"] as SYS_User).WX_App;
+            List<WX_User> list;  
+            if (type != 1)
+            {
+                list = new BaseBLL<WX_User>().Where(u => u.AppId == app.AppId && u.UserId == null);
+            }else
+            {
+                list = new BaseBLL<WX_User>().Where(u => u.AppId == app.AppId);
+            }
+            var json = list.Select(u => new {
+                FollowStatus = (u.UnSubscribeTime == null?"已关注":"已退订"),
+                u.OpenID,
+                u.UserNick,
+                u.HeadImageUrl,
+                u.UserSex,
+                u.City,
+                u.Province,
+                u.Country,
+                u.SubscribeTime,
+                SYS_UserName = (u.UserId==null?"暂无所属客服":u.SYS_User.UserName)
+            });
+            return Json(json, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
--- WXDocument/1数据库/初始化数据.sql
+++ WXDocument/1数据库/初始化数据.sql
@@ -10,6 +10,7 @@ insert into SYS_Role(RoleSign,RoleName)values('APP_CUSTOM','平台客服')
 --Start新增公众号
 insert into WX_App(AppId,AppName,AppSecret,WXId,AppType,AppState,Remark)values('wxb51501fa9702675f','E妹时尚女装','a56e69ded9b5eab3579ce771f2f9a668','gh_f98920d67737','服务号','正常','')
 insert into WX_App(AppId,AppName,AppSecret,WXId,AppType,AppState,Remark)values('wx5e6fd9f5a08db03a','李添乐测试','8f3079b54c134f3e67a88778b4a24f27','gh_ac32a7de334e','服务号','正常','')
+insert into WX_App(AppId,AppName,AppSecret,WXId,AppType,AppState,Remark)values('wxabecfe243f78bfef','谢春永测试','7fa21940796bc96d45abe36a38e65204','gh_29758758c9d8','服务号','正常','')
 
 --End新增公众号
 
@@ -17,5 +18,7 @@ insert into WX_App(AppId,AppName,AppSecret,WXId,AppType,AppState,Remark)values('
 --Start新增用户
 insert into SYS_User(AppId,RoleId,LoginId,Password,UserName,Telphone,Email,WXId,UserState)values('wxb51501fa9702675f',1,'Donald','Donald','Donald','18973305244',null,'gh_f98920d67737','正常')
 insert into SYS_User(AppId,RoleId,LoginId,Password,UserName,Telphone,Email,WXId,UserState)values('wx5e6fd9f5a08db03a',1,'Litle','Litle','Litle','18973305244',null,'gh_ac32a7de334e','正常')
+insert into SYS_User(AppId,RoleId,LoginId,Password,UserName,Telphone,Email,WXId,UserState)values('wxabecfe243f78bfef',1,'xcy','xcy','xcy','18973305244',null,'gh_29758758c9d8','正常')
+
 --End新增用户
 
