fcf59c040341c9d6917aadfefbfa216f859e64e6
linenheng <linjie210@foxmail.com>
2018-12-01 15:25:22 +0800

    555

--- WXDocument/1数据库/初始化数据.sql
+++ WXDocument/1数据库/初始化数据.sql
@@ -14,7 +14,7 @@ insert into WX_App(AppId,AppName,AppSecret,WXId,AppType,AppState,Remark)values('
 
 --End新增公众号
 
-
+ 
 --Start新增用户
 insert into SYS_User(AppId,RoleId,LoginId,Password,UserName,Telphone,Email,WXId,UserState)values('wxb51501fa9702675f',1,'Donald','Donald','Donald','18973305244',null,'gh_f98920d67737','正常')
 insert into SYS_User(AppId,RoleId,LoginId,Password,UserName,Telphone,Email,WXId,UserState)values('wx5e6fd9f5a08db03a',1,'Litle','Litle','Litle','18973305244',null,'gh_ac32a7de334e','正常')
