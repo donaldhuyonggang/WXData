478f6edabea4ade827c08e952c8f35393712e8a4
donaldhuyonggang <65121227@qq.com>
2018-12-04 15:27:56 +0800

    修改扫码逻辑

--- WXDataUI/App_Start/AutoService.cs
+++ WXDataUI/App_Start/AutoService.cs
@@ -68,6 +68,14 @@ namespace WXDataUI.App_Start
                 modal1.UserId = GetUserIdByScene(eventKey);
                 userBLL.Update(modal1);
             }
+            else
+            {
+                var app = appBLL.GetByPK(info.AppId);
+                if(app != null) {
+                    GetUserInfo(info.OpenID, app.AppId, app.AppSecret);
+                }
+                
+            }
          }
 
         public void UnSubscribe(WX_EventQueue info)
@@ -91,58 +99,59 @@ namespace WXDataUI.App_Start
                 CustomService customSvr = new CustomService(app.AppId,app.AppSecret);
                 customSvr.SendText(info.OpenID, "欢迎您关注公众号");
 
-                UserService userSvr = new UserService(app.AppId, app.AppSecret);
+                GetUserInfo(info.OpenID, app.AppId, app.AppSecret);
+            }
+        }
+        public void GetUserInfo(string openId,string appId,string appSecret)
+        {
+            UserService userSvr = new UserService(appId, appSecret);
 
-                string json = userSvr.Info(info.OpenID);
-                JObject jo = JObject.Parse(json);
-                string openId = jo["openid"].ToString();
-                WX_User modal = userBLL.GetByPK(openId);
-                if(modal==null)
-                {
-                    modal = new WX_User();
-                    modal.AppId = info.AppId;
-                    modal.OpenID = openId;
-                    modal.UserNick = jo["nickname"].ToString();
-                    modal.UserSex = jo["sex"].ToString() == "1" ? "男" : "女";
-                    modal.City = jo["city"].ToString();
-                    modal.Country = jo["country"].ToString();
-                    modal.Province = jo["province"].ToString();
-                    modal.Language = jo["language"].ToString();
-                    modal.HeadImageUrl = jo["headimgurl"].ToString();
-                    modal.SubscribeTime = DateTime.Now;
-                    modal.UserState = "正常";
-                    modal.Remark = jo["remark"].ToString();
-                    modal.GrooupId =1;//新用户
-                    modal.Subscribe_Scene = jo["subscribe_scene"].ToString();
-                    modal.QR_Scene = jo["qr_scene"].ToString();
-                    modal.QR_Scene_String =jo["qr_scene_str"].ToString();
-                    modal.UserId = GetUserIdByScene(jo["qr_scene"].ToString());
-                    userBLL.Add(modal);
-                }   
-                else
-                {
-                    WX_User modal1 = new WX_User();
-                    EntityUntility.CopyProperty(modal, modal1);
-                    modal1.AppId = info.AppId;
-                    modal1.OpenID = openId;
-                    modal1.UserNick = jo["nickname"].ToString();
-                    modal1.UserSex = jo["sex"].ToString() == "1" ? "男" : "女";
-                    modal1.City = jo["city"].ToString();
-                    modal1.Country = jo["country"].ToString();
-                    modal1.Province = jo["province"].ToString();
-                    modal1.Language = jo["language"].ToString();
-                    modal1.HeadImageUrl = jo["headimgurl"].ToString();
-                    modal1.UserState = "正常";
-                    modal1.SubscribeTime = DateTime.Now;
-                    modal1.Remark = jo["remark"].ToString();
-                    modal1.Subscribe_Scene = jo["subscribe_scene"].ToString();
-                    modal1.QR_Scene = jo["qr_scene"].ToString();
-                    modal1.QR_Scene_String = jo["qr_scene_str"].ToString();
-                    modal1.UserId = GetUserIdByScene(jo["qr_scene"].ToString());
-                    userBLL.Update(modal1);
-                }
-               
-
+            string json = userSvr.Info(openId);
+            JObject jo = JObject.Parse(json); 
+            WX_User modal = userBLL.GetByPK(openId);
+            if (modal == null)
+            {
+                modal = new WX_User();
+                modal.AppId = appId;
+                modal.OpenID = openId;
+                modal.UserNick = jo["nickname"].ToString();
+                modal.UserSex = jo["sex"].ToString() == "1" ? "男" : "女";
+                modal.City = jo["city"].ToString();
+                modal.Country = jo["country"].ToString();
+                modal.Province = jo["province"].ToString();
+                modal.Language = jo["language"].ToString();
+                modal.HeadImageUrl = jo["headimgurl"].ToString();
+                modal.SubscribeTime = DateTime.Now;
+                modal.UserState = "正常";
+                modal.Remark = jo["remark"].ToString();
+                modal.GrooupId = 1;//新用户
+                modal.Subscribe_Scene = jo["subscribe_scene"].ToString();
+                modal.QR_Scene = jo["qr_scene"].ToString();
+                modal.QR_Scene_String = jo["qr_scene_str"].ToString();
+                modal.UserId = GetUserIdByScene(jo["qr_scene"].ToString());
+                userBLL.Add(modal);
+            }
+            else
+            {
+                WX_User modal1 = new WX_User();
+                EntityUntility.CopyProperty(modal, modal1);
+                modal1.AppId = appId;
+                modal1.OpenID = openId;
+                modal1.UserNick = jo["nickname"].ToString();
+                modal1.UserSex = jo["sex"].ToString() == "1" ? "男" : "女";
+                modal1.City = jo["city"].ToString();
+                modal1.Country = jo["country"].ToString();
+                modal1.Province = jo["province"].ToString();
+                modal1.Language = jo["language"].ToString();
+                modal1.HeadImageUrl = jo["headimgurl"].ToString();
+                modal1.UserState = "正常";
+                modal1.SubscribeTime = DateTime.Now;
+                modal1.Remark = jo["remark"].ToString();
+                modal1.Subscribe_Scene = jo["subscribe_scene"].ToString();
+                modal1.QR_Scene = jo["qr_scene"].ToString();
+                modal1.QR_Scene_String = jo["qr_scene_str"].ToString();
+                modal1.UserId = GetUserIdByScene(jo["qr_scene"].ToString());
+                userBLL.Update(modal1);
             }
         }
         public int? GetUserIdByScene(string scene)
