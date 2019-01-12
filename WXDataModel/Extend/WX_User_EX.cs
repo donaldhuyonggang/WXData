f61891ac318858b51f0f09c95bc3d2e683f909f3
Maple <Maple@MECHREVO-X7TIS>
2019-01-12 12:57:34 +0800

    11

---
+++ WXDataModel/Extend/WX_User_EX.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WXDataModel.Extend
+{
+    public static class WX_User_EX
+    {
+        public static string GetFollowStatus(this WX_User user)
+        {
+            return user.UnSubscribeTime == null ? "已关注" : "已退订";
+        }
+    }
+}
